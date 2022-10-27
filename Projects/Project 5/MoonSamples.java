0d45505cbb4f36977d731050ce009377bbd9c3fc
tankrogers2003 <115171873+tankrogers2003@users.noreply.github.com>
2022-10-27 12:04:51 -0400

    Some more Updates

--- Projects/Project 5/MoonSamples.java
+++ Projects/Project 5/MoonSamples.java
@@ -8,20 +8,26 @@
  * CMSC-255
  */
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class MoonSamples {
     public static void main(String[] args) {
+        //create input
+        Scanner input = new Scanner(System.in);
 
+        String inputString = "8.3,4.5,6.7,2.3,12.5,4.5<>3.9,1.8,34.7,23.5,1.2,14.3<>6.7,7.4,1.5,18.4,7.2,23.7<>23.4,5.6,2.9,18.5,39.5,18.2<>15.4,5.3,27.4,9.8,3.8,27.4";
+        String[] inputTestArray = getElements(inputString);
+        System.out.println(inputTestArray[0]);
     }
     public static String[] getElements(String InputElementString){
        String[] splitString = InputElementString.split(",");
        return splitString;
     }
-    public static double[][] getSamples(String inputSamplesString){
-        inputSamplesString.split("<>");
-        <resulting String>.split(",");
+    /* public static double[][] getSamples(String inputSamplesString){
+        String[] arrayString = inputSamplesString.split("<>");
+        //<resulting String>.split(",");
 
-    }
+    }*/
 
 }
