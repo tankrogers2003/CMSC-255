b300fabcef49792fda2ecd7ed232f012a3236dc2
tankrogers2003 <115171873+tankrogers2003@users.noreply.github.com>
2022-10-27 11:26:47 -0400

    Created MoonSamples.java file

---
+++ Projects/Project 5/MoonSamples.java
@@ -0,0 +1,27 @@
+/*
+ * MoonSamples.java
+ *
+ * A program to help martians analyze the soil on Mars.
+ *
+ * Thomas Rogers
+ * 10/27/2022
+ * CMSC-255
+ */
+
+import java.util.Scanner;
+
+public class MoonSamples {
+    public static void main(String[] args) {
+
+    }
+    public static String[] getElements(String InputElementString){
+       String[] splitString = InputElementString.split(",");
+       return splitString;
+    }
+    public static double[][] getSamples(String inputSamplesString){
+        inputSamplesString.split("<>");
+        <resulting String>.split(",");
+
+    }
+
+}
