6a791a79859101876401db6b397fc5b1f6e427d1
tankrogers2003 <115171873+tankrogers2003@users.noreply.github.com>
2022-11-01 21:30:15 -0400

    finished searchHighestElements method

--- Projects/Project 5/MoonSamples.java
+++ Projects/Project 5/MoonSamples.java
@@ -15,10 +15,6 @@ public class MoonSamples {
     public static void main(String[] args) {
         /**create input*/
         Scanner input = new Scanner(System.in);
-
-        String inputString = "8.3,4.5,6.7,2.3,12.5,4.5<>3.9,1.8,34.7,23.5,1.2,14.3<>6.7,7.4,1.5,18.4,7.2,23.7<>23.4,5.6,2.9,18.5,39.5,18.2<>15.4,5.3,27.4,9.8,3.8,27.4";
-        String[] inputTestArray = getElements(inputString);
-        System.out.println(inputTestArray[0]);
     }
     public static String[] getElements(String InputElementString){
        String[] splitString = InputElementString.split(",");
@@ -26,10 +22,17 @@ public class MoonSamples {
     }
     /**getSamples method. separates string into 2D array*/
     public static double[][] getSamples(String InputSamplesString) {
-        String[] rows = InputSamplesString.split("\n");
+        /**split input String into a single array initially*/
+        String[] rows = InputSamplesString.split("<>");
+
+        /**create a double array, with the same number of rows as the String*/
         double[][] samples = new double[rows.length][];
+
+        /**run a for loop for each row on the array*/
         for (int i = 0; i < rows.length; i++) {
-            String[] rowValues = rows[i].split(" ");
+            /**split array into another array to get the row values*/
+            String[] rowValues = rows[i].split(",");
+
             samples[i] = new double[rowValues.length];
             for (int j = 0; j < rowValues.length; j++) {
                 samples[i][j] = Double.parseDouble(rowValues[j]);
@@ -37,20 +40,66 @@ public class MoonSamples {
         }
         return samples;
     }
+
     public static int[] searchForLife(double [][] samples) {
-        }
-    }
-    public static String searchHighestElements(double[][] samples, String[] elements, int sampleNum) {
-        double highest = samples[0][0];
-        double secondHighest = samples[0][1];
+        /** take in double array of samples */
+        /** search all samples (rows) for those that could support life */
+        /** formulaForLife = 8*carbondioxide +
+         * test the formula for life for EACH sample using a for loop that cycles through each row
+         * inside the loop, have an if statement to check if > 300.
+         * if > 300, place the sample number (row) into a new array. **/
+        int[] lifeSamples = new int[0];
         for (int i = 0; i < samples.length; i++) {
-            if (samples[i][0] > highest) {
-                secondHighest = highest;
-                highest = samples[i][0];
-            } else if (samples[i][0] > secondHighest) {
-                secondHighest = samples[i][0];
+            /** declare and intialize variable used in formulaForLife function*/
+            double carbonDioxide = samples[i][0];
+            double magnesium = samples[i][1];
+            double sodium = samples[i][2];
+            double potassium = samples[i][3];
+            double chloride = samples[i][4];
+            double water = samples[i][5];
+            double formulaForLife = (8 * carbonDioxide) + (2 * magnesium) + sodium + (4 * potassium) + chloride + (5 * water);
+            /** creating new array to samples that pass */
+            if (formulaForLife >= 300) {
+                int j = 0;
+                while (j < samples.length) {
+                    lifeSamples[j] = i;
+                    j++;
+                    break;
+                }
             }
         }
-        return elements[0] + ": " + highest + ", " + elements[1] + ": " + secondHighest;
+        return lifeSamples;
     }
+    public static String searchHighestElements(double[][] samples, String[] elements, int sampleNum) {
+        /** search the given row (from sampleNum) of 2D array samples for the highestSample and secondHighestSample */
+        double highestSample = 0;
+        double secondHighestSample = 0;
+
+        for (int i = 0; i < samples.length; i++) {
+            if (samples[sampleNum][i] > highestSample) {
+                secondHighestSample = highestSample;
+                highestSample = samples[sampleNum][i];
+            } else if (samples[sampleNum][i] > secondHighestSample) {
+                secondHighestSample = samples[sampleNum][i];
+            }
+        }
+        /** find the column location of the highest samples */
+
+        int highestLocation = 0;
+        int secondHighestLocation = 0;
+        for (int i = 0; i < samples.length; i++) {
+            if (samples[sampleNum][i] == highestSample) {
+                highestLocation = i;
+            } else if (samples[sampleNum][i] == secondHighestSample) {
+                secondHighestLocation = i;
+            }
+        }
+        /** find the corresponding string */
+        String highestElement = elements[highestLocation];
+        String secondHighestElement = elements[secondHighestLocation];
+
+        /** return string Elements =  highestElement + " " + secondHighestElement */
+        return highestElement + " " + secondHighestElement;
+    }
+    
 }
