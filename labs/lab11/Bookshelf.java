0785f728270e6b77198b2e44d325e75503461650
tankrogers2003 <115171873+tankrogers2003@users.noreply.github.com>
2022-11-02 16:21:27 -0400

    Working on lab11

---
+++ labs/lab11/Bookshelf.java
@@ -0,0 +1,78 @@
+import java.util.ArrayList;
+
+public class Bookshelf {
+    /**
+     * This code is creating a Bookshelf object with a specified size
+     */
+    private int size;
+    private ArrayList<Book> books;
+
+    public Bookshelf() {
+        size = 2;
+        books = new ArrayList<Book>();
+    }
+
+    public Bookshelf(int size) {
+        this.size = size;
+        books = new ArrayList<Book>();
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public ArrayList<Book> getBooks() {
+        return books;
+    }
+
+    /**
+     * If the bookshelf is full, then a message is printed saying that the bookshelf is full.
+     */
+    public void add(Book book) {
+        if (books.size() == size) {
+            System.out.println("This bookshelf is full");
+        }
+        else {
+            books.add(book);
+        }
+    }
+
+    /**
+     * This code is iterating through a list of books and printing the title and author of each book.
+     */
+    public void print() {
+        for (int i = 0; i < books.size(); i++) {
+            System.out.println(books.get(i).getTitle() + " by " + books.get(i).getAuthor());
+        }
+    }
+
+    /**
+     * A method which takes a single book parameter and adds it to the books ArrayList if there is room on the bookshelf.
+     */
+    public void addBook(Book book){
+        //confused on max number of books
+        //ask TA
+        if(books.size() < Bookshelf().size){
+            books.add(book);
+        }
+    }
+
+    /**
+     * A method which takes no parameters, removes the first book on the bookshelf (the book at position 0 in the books ArrayList), and returns it (If there are no books in the bookshelf, then it returns null.).
+     */
+    public Book removeBook(){
+        if(books.size() > 0){
+            return books.remove(0);
+        }
+        else{
+            return null;
+        }
+    }
+
+    /**
+     * A public void method emptyBookshelf, which takes no parameters and removes all of the books from the bookshelf (empties the books ArrayList).
+     */
+    public void emptyBookshelf(){
+        books.clear();
+    }
+}
---
+++ labs/lab11/Lab11.java
@@ -0,0 +1,45 @@
+public class Lab11 {
+    public static void main(String[] args) {
+
+        /**
+         * create three book objects
+         */
+        Book b1 = new Book("50 Shades of Grey", "E. L. James");
+        Book b2 = new Book("The Communist Manifesto", "Karl Marx");
+        Book b3 = new Book("Kama Sutra", "Vatsyayana Mallanaga");
+
+        /**
+         * print data from books
+         */
+        System.out.println(b1.getAuthor() + ", " + b1.getTitle());
+        System.out.println(b2.getAuthor() + ", " + b2.getTitle());
+        System.out.println(b3.getAuthor() + ", " + b3.getTitle());
+
+        /**
+         * create bookshelf object with three book objects in it
+         */
+        Bookshelf bkS1 = new Bookshelf();
+        bkS1.addBook(b1);
+        bkS1.addBook(b2);
+        bkS1.addBook(b3);
+
+        /**
+         * enhanced for loop to display contents of book objects in books ArrayList
+         */
+        //ask TA
+        for (Book newBook : bkS1.getBooks()) {
+            System.out.println(newBook.getAuthor() + "," + newBook.getTitle());
+        }
+
+        /**
+         * empty bookshelf and print results
+         */
+        //ask TA
+        bkS1.emptyBookshelf();
+        for (Book newBook : bkS1.getBooks()) {
+            System.out.println(newBook.getAuthor() + "," + newBook.getTitle());
+        }
+
+        System.out.println(bkS1.toString());
+    }
+}
